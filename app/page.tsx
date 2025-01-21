d358f59b5316fc3d17f748e4bc54dc51279e7134
Great Ogheneochuko <greatochuko123@gmail.com>
2025-01-21 10:08:23 +0100

    Add StackSection component to display technology stack; include logos and descriptions for TypeScript, React, NodeJS, and Tailwind CSS

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import AboutSection from "@/components/AboutSection";
 import RecentProjectSection from "@/components/RecentProjectSection";
+import StackSection from "@/components/StackSection";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-12 py-0">
       <AboutSection />
       <RecentProjectSection />
+      <StackSection />
     </div>
   );
 }
---
+++ components/StackSection.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import typescriptLogo from "@/public/typescript-logo.png";
+import nodeJSLogo from "@/public/nodejs-icon.svg";
+import reactLogo from "@/public/react-logo.png";
+import tailwindLogo from "@/public/tailwind-logo.svg";
+import Image from "next/image";
+import ViewAllButton from "./ViewAllButton";
+
+const techStack = [
+  {
+    name: "TypeScript",
+    description: "Typed Javascript",
+    logo: typescriptLogo,
+  },
+  {
+    name: "React",
+    description: "JavaScript UI library",
+    logo: reactLogo,
+  },
+  {
+    name: "NodeJS",
+    description: "JavaScript runtime",
+    logo: nodeJSLogo,
+  },
+  {
+    name: "Tailwind CSS",
+    description: "Utility-first CSS framework",
+    logo: tailwindLogo,
+  },
+];
+
+export default function StackSection() {
+  return (
+    <div className="flex flex-col gap-8">
+      <h2 className="text-2xl font-medium">Stack</h2>
+      <div className="grid gap-6 sm:grid-cols-2">
+        {techStack.map((stack) => (
+          <div
+            key={stack.name}
+            className="flex flex-1 items-center gap-4 rounded-lg bg-muted p-6"
+          >
+            <Image
+              src={stack.logo}
+              alt={stack.name + " logo"}
+              className="h-12 w-12 rounded-md object-contain"
+            />
+            <div className="flex flex-col">
+              <h3 className="text-lg font-medium">{stack.name}</h3>
+              <p className="text-sm text-accent-gray">{stack.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="flex flex-wrap items-center gap-4 text-sm text-accent-gray">
+        <hr className="hidden flex-1 border-dashed border-[#666] sm:block" />
+        <ViewAllButton href="/stack" className="mt-0">
+          View All Stack
+        </ViewAllButton>
+      </div>
+    </div>
+  );
+}
